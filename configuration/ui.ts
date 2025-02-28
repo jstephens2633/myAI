9ec77005a23b22329c3783a4b7cc4f69ba80de79
jstephens2633 <jstephens2633@gmail.com>
2025-02-28 11:11:21 -0500

    Update ui.ts
    changed name

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,7 +1,7 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `[CHAT HEADER]`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const MESSAGE_PLACEHOLDER: string = `Welcome to H Street Digital`;
 export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
